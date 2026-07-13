8dda79cf048ed88e7c544196fa4ede4bbc4a7e58
bastian joaquin navarro gorena <alpasinojaja@gmail.com>
2026-07-12 23:03:54 -0400

    Agregar credenciales a los fetch del login local

--- android/app/src/main/java/com/tupatente/app/MainActivity.java
+++ android/app/src/main/java/com/tupatente/app/MainActivity.java
@@ -1,16 +1,5 @@
 package com.tupatente.app;
 
-import android.os.Bundle;
-import android.webkit.CookieManager; // Importante: importar esto
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Habilitar cookies de terceros (crucial para la persistencia de sesión)
-        CookieManager.getInstance().setAcceptThirdPartyCookies(getWebView(), true);
-    }
-}
+public class MainActivity extends BridgeActivity {}
